a6ec198610abf45df66e373380658dd26fd7407c
nickofolas <60761231+nickofolas@users.noreply.github.com>
2021-05-29 12:07:10 -0500

    Use &str instead of String for client_id

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ use ipc_windows as ipc;
 
 /// Creates a new client to connect to the Discord IPC.
 /// This method automatically creates and connects a client.
+pub fn new_client(client_id: &str) -> Result<impl DiscordIpc, Box<dyn std::error::Error>> {
     let mut client = ipc::DiscordIpcClient {
         connected: false,
         socket: None,
