1eb92db4c791232ae906e8a0535279b8f062adf8
nickofolas <nickofolas@gmail.com>
2022-03-08 11:01:20 -0600

    Update docs: - README.md - Minor rustfmt - Update DiscordIpc trait docs

--- src/discord_ipc.rs
+++ src/discord_ipc.rs
@@ -9,18 +9,20 @@ use uuid::Uuid;
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 /// A client that connects to and communicates with the Discord IPC.
+///
+/// Implemented via the [`DiscordIpcClient`](struct@crate::DiscordIpcClient) struct.
 pub trait DiscordIpc {
     /// Connects the client to the Discord IPC.
-    /// 
+    ///
     /// This method attempts to first establish a connection,
     /// and then sends a handshake.
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// Returns an `Err` variant if the client
     /// failed to connect to the socket, or if it failed to
     /// send a handshake.
-    /// 
+    ///
     /// # Examples
     /// ```
     /// let mut client = discord_rich_presence::new_client("<some client id>")?;
@@ -37,18 +39,18 @@ pub trait DiscordIpc {
     ///
     /// This method closes the client's active connection,
     /// then re-connects it and re-sends a handshake.
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// Returns an `Err` variant if the client
     /// failed to connect to the socket, or if it failed to
     /// send a handshake.
-    /// 
+    ///
     /// # Examples
     /// ```
     /// let mut client = discord_rich_presence::new_client("<some client id>")?;
     /// client.connect()?;
-    /// 
+    ///
     /// client.close()?;
     /// client.reconnect()?;
     /// ```
@@ -67,16 +69,16 @@ pub trait DiscordIpc {
     fn connect_ipc(&mut self) -> Result<()>;
 
     /// Handshakes the Discord IPC.
-    /// 
+    ///
     /// This method sends the handshake signal to the IPC.
     /// It is usually not called manually, as it is automatically
     /// called by [`connect`] and/or [`reconnect`].
-    /// 
+    ///
     /// [`connect`]: #method.connect
     /// [`reconnect`]: #method.reconnect
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// Returns an `Err` variant if sending the handshake failed.
     fn send_handshake(&mut self) -> Result<()> {
         self.send(
@@ -93,13 +95,13 @@ pub trait DiscordIpc {
     }
 
     /// Sends JSON data to the Discord IPC.
-    /// 
+    ///
     /// This method takes data (`serde_json::Value`) and
     /// an opcode as its parameters.
-    /// 
+    ///
     /// # Errors
     /// Returns an `Err` variant if writing to the socket failed
-    /// 
+    ///
     /// # Examples
     /// ```
     /// let payload = serde_json::json!({ "field": "value" });
@@ -119,19 +121,19 @@ pub trait DiscordIpc {
     fn write(&mut self, data: &[u8]) -> Result<()>;
 
     /// Receives an opcode and JSON data from the Discord IPC.
-    /// 
+    ///
     /// This method returns any data received from the IPC.
     /// It returns a tuple containing the opcode, and the JSON data.
-    /// 
+    ///
     /// # Errors
     /// Returns an `Err` variant if reading the socket was
     /// unsuccessful.
-    /// 
+    ///
     /// # Examples
     /// ```
     /// client.connect_ipc()?;
     /// client.send_handshake()?;
-    /// 
+    ///
     /// println!("{:?}", client.recv()?);
     /// ```
     fn recv(&mut self) -> Result<(u32, Value)> {
@@ -153,13 +155,13 @@ pub trait DiscordIpc {
     fn read(&mut self, buffer: &mut [u8]) -> Result<()>;
 
     /// Sets a Discord activity.
-    /// 
+    ///
     /// This method is an abstraction of [`send`],
     /// wrapping it such that only an activity payload
     /// is required.
-    /// 
+    ///
     /// [`send`]: #method.send
-    /// 
+    ///
     /// # Errors
     /// Returns an `Err` variant if sending the payload failed.
     fn set_activity(&mut self, activity_payload: Activity) -> Result<()> {
