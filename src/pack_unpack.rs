6cab60ed1c40d3e4d5269bcd1c0458615fd4fc65
sardonicism-04 <sardony01@gmail.com>
2023-03-15 12:19:40 -0400

    Refactor pack_unpack

--- src/pack_unpack.rs
+++ src/pack_unpack.rs
@@ -1,18 +1,18 @@
+use std::array::TryFromSliceError;
 use std::convert::TryInto;
-use std::error::Error;
 
 // Re-implement some packing methods in Rust
-pub fn pack(opcode: u32, data_len: u32) -> Result<Vec<u8>, Box<dyn Error>> {
+pub fn pack(opcode: u32, data_len: u32) -> Vec<u8> {
     let mut bytes = Vec::new();
 
     for byte_array in &[opcode.to_le_bytes(), data_len.to_le_bytes()] {
         bytes.extend_from_slice(byte_array);
     }
 
-    Ok(bytes)
+    bytes
 }
 
-pub fn unpack(data: Vec<u8>) -> Result<(u32, u32), Box<dyn Error>> {
+pub fn unpack(data: Vec<u8>) -> Result<(u32, u32), TryFromSliceError> {
     let data = data.as_slice();
     let (opcode, header) = data.split_at(std::mem::size_of::<u32>());
 
